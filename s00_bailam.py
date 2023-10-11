0385602e3b0a5d7fa51b8d3416d35c3cbe037e1d
namgivu <namgivu@gmail.com>
2023-10-11 15:03:12 +0000

    debai typo

--- s00_bailam.py
+++ s00_bailam.py
@@ -24,15 +24,51 @@ hi('Mom')                    | Hi Mom!
 hi('')                       | Hi!
 hi()                         | Hi!
 hi(None)                     | Hi!
+hi(name='Mom')               | Hi Mom!
 
 ------------------- mucdo: kho -----------------
 hi('Mom', 'Dad')             | Hi Mom, and Dad!
 hi('A', 'B', 'C')            | Hi A, B, and C!
-hi('1', '22', '333', '4444') | Hi A, B, and C!
+hi('1', '22', '333', '4444') | Hi 1, 22, 333 and 4444!
 """
 #endregion debai
 
 #region bailam
-def hi(name):
-  return 'todo'
+#             **kwargs to allow calling hi(name='xx') without error > TypeError: hi() got an unexpected keyword argument 'name'
+def hi(*args, **kwargs):
+  if len(args)==0:  # hi()  --> args = (,)
+    if not kwargs:
+      return 'Hi!'
+    else:
+      name = kwargs.get('name')
+      if name:
+        return f'Hi {name}!'
+      else:
+        return 'Hi!'
+
+  if len(args)==1 and args[0] is None:  # hi(None)  --> args = (None,)
+    return 'Hi!'
+
+  if len(args)==1:  # hi('Mom') hi('')
+    name = args[0]
+    if name:
+      return f'Hi {name}!'
+    else:
+      return 'Hi!'
+
+  if len(args)>1:  # hi('Mom', 'Dad')
+    namestr = ', '.join(args[0:-1])
+    namestr = f'{namestr}, and {args[-1]}'
+    return f"Hi {namestr}!"
+
+if __name__=='__main__':
+  print( hi('Mom') )  # Hi Mom!
+  print( hi()      )  # Hi!
+  print( hi(None)  )  # Hi!
+
+  print( hi('Mom', 'Dad') )                # Hi Mom, and Dad!
+  print( hi('A', 'B', 'C')  )              # Hi A, B, and C!
+  print( hi('1', '22', '333', '4444')   )  # Hi 1, 22, 333 and 4444!
+  
+  print( hi(name='Mom')   )  # Hi Mom!
 #endregion bailam
